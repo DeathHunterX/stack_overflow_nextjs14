66b775d6cf232e362a3fb3c474bf160c13dd9811
Loi Phan <88315858+DeathHunterX@users.noreply.github.com>
2024-09-09 16:46:56 +0700

    add basic navbar and install shadcn-ui

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
