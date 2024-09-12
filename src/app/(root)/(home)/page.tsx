1c0e3b0e6adcf3cc72c9beba80e77ae6e08ab9c8
Loi Phan <88315858+DeathHunterX@users.noreply.github.com>
2024-09-12 18:21:25 +0700

    add question cards with dummy data

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,5 +1,7 @@
+import QuestionCard from "@/components/shared/cards/QuestionCard";
 import Filter from "@/components/shared/Filter";
 import HomeFilters from "@/components/shared/home/HomeFilters";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
@@ -7,6 +9,43 @@ import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
+  const questions = [
+    {
+      _id: "1",
+      title: "Cascading Deletes in SQLAlchemy?",
+      tags: [
+        { _id: "1", name: "python" },
+        { _id: "2", name: "sql" },
+      ],
+      author: {
+        _id: "101",
+        name: "John Doe",
+        picture: "/assets/icons/avatar.svg",
+      },
+      upvotes: 10,
+      views: 100,
+      answers: [{}, {}], // Representing 2 answers as objects
+      createdAt: new Date("2021-09-01T12:00:00.000Z"),
+    },
+    {
+      _id: "2",
+      title: "How to center a div?",
+      tags: [
+        { _id: "3", name: "html" },
+        { _id: "4", name: "css" },
+      ],
+      author: {
+        _id: "102",
+        name: "Jane Smith",
+        picture: "/assets/icons/avatar.svg",
+      },
+      upvotes: 15,
+      views: 200,
+      answers: [{}], // Representing 1 answer as an object
+      createdAt: new Date("2021-09-01T12:00:00.000Z"),
+    },
+  ];
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -35,6 +74,33 @@ const HomePage = () => {
         />
       </div>
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+        discussion. our query could be the next big thing others learn from. Get
+        involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
