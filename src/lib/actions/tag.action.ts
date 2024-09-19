f31572348bbaccc6731d5bc6ed6edb090ad86b71
Loi Phan <88315858+DeathHunterX@users.noreply.github.com>
2024-09-19 16:29:48 +0700

    add tag page

--- src/lib/actions/tag.action.ts
+++ src/lib/actions/tag.action.ts
@@ -1,8 +1,9 @@
 "use server";
 
-import User from "@/models/user.model";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 import { connectToDatabase } from "../db/mongoose";
+import User from "@/models/user.model";
+import Tag from "@/models/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -21,6 +22,21 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag2" },
       { _id: "3", name: "tag3" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const tags = await Tag.find({}).sort({ createdAt: -1 });
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
